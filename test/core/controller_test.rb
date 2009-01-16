97f12dc2981ec4bce599b84121fc9ac595b49bfa
Cliff Hall <cliff.hall@puremvc.org>
2009-01-16 01:12:22 +0000

    added attribution to bottom of files

--- test/core/controller_test.rb
+++ test/core/controller_test.rb
@@ -75,6 +75,3 @@ end
 # PureMVC Port to Ruby by Jake Dempsey <jake.dempsey@puremvc.org>
 # PureMVC - Copyright(c) 2006-2008 Futurescale, Inc., Some rights reserved.
 # Your reuse is governed by the Creative Commons Attribution 3.0 License
-# PureMVC Port to Ruby by Jake Dempsey <jake.dempsey@puremvc.org>
-# PureMVC - Copyright(c) 2006-2008 Futurescale, Inc., Some rights reserved.
-# Your reuse is governed by the Creative Commons Attribution 3.0 License
