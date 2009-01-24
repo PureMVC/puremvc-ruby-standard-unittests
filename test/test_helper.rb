67e730e4d00c068275febafcdbc150c5db65e404
Jake Dempsey <jake.dempsey@puremvc.org>
2009-01-24 03:39:16 +0000

    should not have removed externals on unit tests..trying to fix

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,5 @@
 require 'test/unit'
-require 'rubygems'
-require 'puremvc-ruby'
+require File.join(File.dirname(__FILE__),'..','lib/PureMVC_Ruby/init')
 # PureMVC Port to Ruby by Jake Dempsey <jake.dempsey@puremvc.org>
 # PureMVC - Copyright(c) 2006-2008 Futurescale, Inc., Some rights reserved.
 # Your reuse is governed by the Creative Commons Attribution 3.0 License
