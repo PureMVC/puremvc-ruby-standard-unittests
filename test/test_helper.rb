4d877459f4f671eb4ebec156983c8615a0cc540f
Jake Dempsey <jake.dempsey@puremvc.org>
2009-01-24 03:28:42 +0000

    updaing testhelper to use new gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 require 'test/unit'
-require File.join(File.dirname(__FILE__),'..','lib/PureMVC_Ruby/init')
+require 'rubygems'
+require 'puremvc-ruby'
 # PureMVC Port to Ruby by Jake Dempsey <jake.dempsey@puremvc.org>
 # PureMVC - Copyright(c) 2006-2008 Futurescale, Inc., Some rights reserved.
 # Your reuse is governed by the Creative Commons Attribution 3.0 License
